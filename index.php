ac371a4f6eef3dcaa1769ebc4cbc190c298e1acc
Richard Ueckermann <rayruok@gmail.com>
2016-08-28 10:26:48 +0200

    created files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Rosslynne & Richard</title>
+
+    <link href="Content/site.css" rel="stylesheet" />
+    <script src="Scripts/jquery-1.10.2.js"></script>
+    <!--<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>-->
+    <script src="Scripts/fireflies.js"></script>
+</head>
+<body>
+<div id="container">
+    <canvas id="pixie"></canvas>
+</div>
+</body>
+</html>
