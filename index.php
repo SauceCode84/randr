25b71be002297bf95218e46c11d7d638ec399bb8
Richard Ueckermann <rayruok@gmail.com>
2016-08-28 10:38:52 +0200

    added fireflies script code

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Rosslynne & Richard</title>
 
-    <link href="Content/site.css" rel="stylesheet" />
-    <script src="Scripts/jquery-1.10.2.js"></script>
-    <!--<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>-->
-    <script src="Scripts/fireflies.js"></script>
+    <link href="assets/site.css" rel="stylesheet" />
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="scripts/fireflies.js"></script>
 </head>
 <body>
 <div id="container">
