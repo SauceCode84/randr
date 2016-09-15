6507d5b633af62e5184c6c8f57378c564f45fdbc
Richard Ueckermann <rueckermann@sse.co.za>
2016-09-15 14:28:29 +0200

    fixed contact form added months to countdown - using momentjs added new favicon added "save the date" image

--- temp/contact-form.php
+++ temp/contact-form.php
@@ -1,5 +1,9 @@
 <?php
 
+//error_reporting(-1);
+//ini_set('display_errors', 'On');
+//set_error_handler('var_dump');
+
 // check for form submission - if it doesn�t exist then send back to contact form
 if (!isset($_POST["save"]) || $_POST["save"] != "contact-form") {
     header("Location: .html#rsvp"); exit;
@@ -11,14 +15,33 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $attending = $_POST['radio_group'];
 
+// setup the headers
+//$to = "rayruok@gmail.com";
+$to = "ourwedding@rosslynneandrichard.com";
+
+$headers = array(
+    "From: $name <$email>",
+    "Reply-To: ourwedding@rosslynneandrichard.com",
+    "X-Mailer: PHP/" . PHP_VERSION
+    );
+
+$headers = implode("\r\n", $headers);
+
+$subject = "RSVP from $name";
+
 // write the email content
 $email_content .= "Name: $name\n";
 $email_content .= "Email: $email\n";
-$email_content .= "Message:\n\n$message";
-$email_content .= "Attending:\n\n$attending";
+$email_content .= "Attending: $attending\n\n";
+$email_content .= "Message: $message";
+
+//echo $to;
+//echo $headers;
+//echo $email_content;
 
 // send the email
-mail ("contact@ouremail.com", "New Contact Message", $email_content);
+//mail ("ourwedding@rosslynneandrichard.com", "New Contact Message", $email_content);
+mail ($to, $subject, $email_content, $headers);
 
 // send the user back to the form
 header("Location: index.html#rsvp"); exit;
--- temp/js/countdown.js
+++ temp/js/countdown.js
@@ -20,6 +20,14 @@
 		//main countdown function
 		function countdown_proc() {
 			
+			var moCurrent = moment();
+			var moBigDay = moment(settings.date);
+			var diff = moment.duration(moBigDay.diff(moCurrent));
+
+			//console.log(moCurrent);
+			//console.log(moBigDay);
+			//console.log(diff);
+
 			var eventDate = Date.parse(settings.date) / 1000;
 			var currentDate = Math.floor($.now() / 1000);
 			
@@ -29,38 +37,36 @@
 				clearInterval(interval);
 			}
 			
-            var seconds = eventDate - currentDate;
-			
-			var days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
-			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
-			
-			var hours = Math.floor(seconds / (60 * 60));
-			seconds -= hours * 60 * 60; //update the seconds variable with no. of hours removed
-			
-			var minutes = Math.floor(seconds / 60);
-			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
-			
+			var seconds = diff.seconds();
+			var minutes = diff.minutes();
+			var hours = diff.hours();
+			var days = diff.days();
+			var months = diff.months();
+
 			//conditional Ss
             /*jshint validthis:true */
-			if (days === 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
-			if (hours === 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
-			if (minutes === 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
 			if (seconds === 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
-			
+			if (minutes === 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
+			if (hours === 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
+			if (days === 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
+			if (months === 1) { thisEl.find(".timeRefMonths").text("month"); } else { thisEl.find(".timeRefMonths").text("months"); }
+						
 			//logic for the two_digits ON setting
 			if(settings.format === "on") {
-				days = (String(days).length >= 2) ? days : "0" + days;
-				hours = (String(hours).length >= 2) ? hours : "0" + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
 				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
+				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
+				hours = (String(hours).length >= 2) ? hours : "0" + hours;
+				days = (String(days).length >= 2) ? days : "0" + days;
+				months = (String(months).length >= 2) ? months : "0" + months;
 			}
 			
 			//update the countdown's html values.
 			if(!isNaN(eventDate)) {
-				thisEl.find(".days").text(days);
-				thisEl.find(".hours").text(hours);
-				thisEl.find(".minutes").text(minutes);
 				thisEl.find(".seconds").text(seconds);
+				thisEl.find(".minutes").text(minutes);
+				thisEl.find(".hours").text(hours);
+				thisEl.find(".days").text(days);
+				thisEl.find(".months").text(months);
 			} else { 
 				clearInterval(interval); 
 			}
@@ -70,7 +76,6 @@
 		countdown_proc();
 		
 		//loop the function
-		interval = setInterval(countdown_proc, 1000);
-		
+		interval = setInterval(countdown_proc, 1000);		
     };
 }) (jQuery);
